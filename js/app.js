70ec7748448c307d8fecc33ade966543d6e00e8f
Michael Friedman <contact@michaeldavidfriedman.com>
2016-11-17 15:43:44 -0800

    Some Progress...

--- js/app.js
+++ js/app.js
@@ -1,8 +1,41 @@
 (function () {
   'use strict';
-  $('span').on('click', (event) => {
-    console.log('jquery here.')
+  let result = 0;
+  let previousEntry = 0;
+  let operation = null;
+  $('.buttons').on('click', 'span', (event) => {
+    const $event = $(event.target)
+    console.log($event.hasClass('operator'))
+    if ($event.hasClass('operator')) {
+      operation = $event.text()
+      console.log(operation)
+      return operation
+    }
+    $('#screen').append(parseInt($event.text()));
+    // console.log(result)
   })
-  // const nine = $('span').eq(9).text();
-  // console.log(9)
+
+  const sum = ((operation) => {
+    if (operation === '+') {
+      return a + b;
+    }
+  })
+
+  const divide = ((operation) => {
+    if (operation === '÷') {
+      return a / b;
+    }
+  })
+
+  const product = ((operation) => {
+    if (operation === '*') {
+      return a * b;
+    }
+  })
+
+  const subtract = ((operation) => {
+    if (operation === '-') {
+      return a - b;
+    }
+  })
 })();
