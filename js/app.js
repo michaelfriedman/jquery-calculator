3bb73434a3167f4ff0454dcdaed4797772c5e173
Michael Friedman <contact@michaeldavidfriedman.com>
2016-11-23 13:29:10 -0800

    JavaScript math.

--- js/app.js
+++ js/app.js
@@ -1,41 +1,67 @@
-(function () {
+(function() {
   'use strict';
-  let result = 0;
-  let previousEntry = 0;
-  let operation = null;
+
+  const $screen = $('#screen');
+  const clear = () => $('#screen').text('');
+  const sum = (lo, ro) => lo + ro;
+  const divide = (lo, ro) => lo / ro;
+  const product = (lo, ro) => lo * ro;
+  const subtract = (lo, ro) => lo - ro;
+
+  $('#clear').on('click', () => {
+    clear();
+  });
+
+  $('#equals').on('click', () => {
+    const screen = $screen.text();
+
+    if (screen === 'Error') {
+      return;
+    }
+
+    const regexp = /^(\-?\d+)(\+|\-|x|÷)(\-?\d+)$/;
+    const matches = screen.match(regexp);
+
+    if (matches === null) {
+      $screen.text('Error');
+
+      return;
+    }
+    const leftOperand = parseInt(matches[1]);
+    const rightOperand = parseInt(matches[3]);
+    const operator = matches[2];
+    let result;
+
+    if (operator === '+') {
+      result = sum(leftOperand, rightOperand);
+      $screen.text(result);
+    }
+    else if (operator === '-') {
+      result = subtract(leftOperand, rightOperand);
+      $screen.text(result);
+    }
+    else if (operator === 'x') {
+      result = product(leftOperand, rightOperand);
+      $screen.text(result);
+    }
+    else if (operator === '÷') {
+      if (rightOperand === 0) {
+        $screen.text('Error');
+        return;
+      }
+      result = divide(leftOperand, rightOperand);
+      $screen.text(result);
+    }
+    const nextScreen = result.toString();
+    $screen.text(nextScreen);
+  });
+
   $('.buttons').on('click', 'span', (event) => {
-    const $event = $(event.target)
-    console.log($event.hasClass('operator'))
-    if ($event.hasClass('operator')) {
-      operation = $event.text()
-      console.log(operation)
-      return operation
+    const screen = $screen.text();
+    if (screen === 'Error') {
+      return;
     }
-    $('#screen').append(parseInt($event.text()));
-    // console.log(result)
-  })
-
-  const sum = ((operation) => {
-    if (operation === '+') {
-      return a + b;
-    }
-  })
-
-  const divide = ((operation) => {
-    if (operation === '÷') {
-      return a / b;
-    }
-  })
-
-  const product = ((operation) => {
-    if (operation === '*') {
-      return a * b;
-    }
-  })
-
-  const subtract = ((operation) => {
-    if (operation === '-') {
-      return a - b;
-    }
-  })
+    const nextScreen = screen + event.target.textContent;
+    $screen.text(nextScreen);
+  });
 })();
